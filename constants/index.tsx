792bfb360c796e5c72defba7d5669f1f9fa7382b
Harshit01sh <106943610+Harshit01sh@users.noreply.github.com>
2024-11-01 13:29:05 +0530

    Initial commit

---
+++ constants/index.tsx
@@ -0,0 +1,145 @@
+import { RxHome, RxPerson, RxDashboard, RxClipboard } from "react-icons/rx";
+
+export const SkillData = [
+  {
+    name: "Html 5",
+    Image: "/html.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    name: "Css",
+    Image: "/css.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    name: "JavaScript",
+    Image: "/js.png",
+    width: 65,
+    height: 65,
+  },
+  {
+    name: "Tailwind Css",
+    Image: "/tailwind.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    name: "React",
+    Image: "/react.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    name: "Redux",
+    Image: "/redux.png",
+    width: 80,
+    height: 80,
+  },
+
+  {
+    name: "TypeScript",
+    Image: "/ts.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    name: "Next js 13",
+    Image: "/next.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    name: "Framer Motion",
+    Image: "/framer.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    name: "Stripe Payment",
+    Image: "/stripe.webp",
+    width: 80,
+    height: 80,
+  },
+  {
+    name: "Node js",
+    Image: "/node-js.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    name: "Mongo db",
+    Image: "/mongodb.png",
+    width: 40,
+    height: 40,
+  },
+];
+
+export const Socials = [
+  {
+    name: "Instagram",
+    src: "/instagram.svg",
+    url: "https://www.instagram.com/harshit01sh/profilecard/?igsh=MXQ4d3gzYTFuMmxldg==",
+  },
+  {
+    name: "Facebook",
+    src: "/facebook.svg",
+    url: "https://www.facebook.com/share/1Xh6dDfchY/",
+  },
+  {
+    name: "github",
+    src: "/github.svg",
+    url:"https://github.com/Harshit01sh"
+  },
+  {
+    name: "LinkeDin",
+    src: "/linkedin.svg",
+    url:"https://www.linkedin.com/in/harshit-sharma-3326ab225",
+  },
+];
+export const Projects = [
+  {
+    title: "Modern Nextjs Website",
+    text: "Our modern website project is crafted to deliver a sleek, intuitive, and engaging user experience. Built with cutting-edge technologies.",
+    src: "/NextWebsite.png",
+  },
+  {
+    title: "Space Themed Website",
+    text: "Immerse yourself in a universe of endless possibilities. Our space-themed website invites you to journey beyond the ordinary, where exploration meets innovation.",
+    src: "/SpaceWebsite.png",
+  },
+  {
+    title: "Modern Nextjs Portfolio",
+    text: "Discover a world where creativity meets functionality. With a focus on design excellence and innovative solutions,I ensure that every detail is crafted to perfection.",
+    src: "/WebPortfolio.png",
+  },
+  {
+    title: "E Learning App",
+    text: "Our e-learning app, built with React Native, offers a seamless and engaging way to acquire new knowledge. Designed to inspire and empower learners.",
+    src: "/elearning.png",
+  },
+];
+
+export const NavLinks = [
+  {
+    name: "/",
+    icon: RxHome,
+    link: "/",
+  },
+  {
+    name: "/my-skills",
+    icon: RxPerson,
+    link: "/my-skills",
+  },
+  {
+    name: "/my-projects",
+    icon: RxDashboard,
+    link: "/my-projects",
+  },
+  {
+    name: "/contact-me",
+    icon: RxClipboard,
+    link: "/contact-me",
+  },
+];
